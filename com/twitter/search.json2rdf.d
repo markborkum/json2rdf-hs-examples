06521624479a8802a6983e934f4ab9847e09dc04
Mark Borkum <m.i.borkum@soton.ac.uk>
2013-05-08 17:18:11 +0100

    Added join descriptor tree

---
+++ com/twitter/search.json2rdf.d
@@ -0,0 +1,56 @@
+{
+  "results" : [
+    {
+      "created_at" : ┴,
+      "entities" : {
+        "hashtags" : [
+          {
+            "text" : ┴
+          }
+        ],
+        "media" : [
+          {
+            "display_url" : ┴,
+            "expanded_url" : ┴,
+            "id" : ┴,
+            "media_url" : ┴,
+            "media_url_https" : ┴,
+            "url" : ┴
+          }
+        ],
+        "urls" : [
+          {
+            "display_url" : ┴,
+            "expanded_url" : ┴,
+            "url" : ┴
+          }
+        ],
+        "user_mentions" : [
+          {
+            "id" : ┴,
+            "name" : ┴,
+            "screen_name" : ┴
+          }
+        ]
+      },
+      "from_user" : ┴,
+      "from_user_id" : ┴,
+      "from_user_name" : ┴,
+      "geo" : {
+        "coordinates" : [
+          0 : ┴,
+          1 : ┴
+        ]
+      },
+      "id" : ┴,
+      "iso_language_code" : ┴,
+      "profile_image_url" : ┴,
+      "profile_image_url_https" : ┴,
+      "source" : ┴,
+      "text" : ┴,
+      "to_user" : ┴,
+      "to_user_id" : ┴,
+      "to_user_name" : ┴
+    }
+  ]
+}
